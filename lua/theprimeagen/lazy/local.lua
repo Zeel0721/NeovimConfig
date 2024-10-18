c5e4b8cc3c70a0f924d03b93b516f2a662d3d5fd
Zeel0721 <zeelpatel0721@gmail.com>
2024-10-18 10:05:52 +0530

    nvim config

---
+++ lua/theprimeagen/lazy/local.lua
@@ -0,0 +1,42 @@
+local local_plugins = {
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        end
+    },
+    {
+        "ThePrimeagen/vim-apm",
+        config = function()
+            --[[
+            local apm = require("vim-apm")
+
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+            --]]
+        end
+    },
+    {
+        "ThePrimeagen/vim-with-me",
+        config = function() end
+    },
+}
+
+return local_plugins
